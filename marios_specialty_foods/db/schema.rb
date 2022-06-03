72af1b5f00e6f56aeaf62f5284b69c9c28478994
Rob Sameh <rob.sameh@gmail.com>
2022-06-02 17:55:13 -0700

    created relationship between one product and many reviews

--- marios_specialty_foods/db/schema.rb
+++ marios_specialty_foods/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_004116) do
+ActiveRecord::Schema.define(version: 2022_06_03_005153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,14 @@ ActiveRecord::Schema.define(version: 2022_06_03_004116) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "author"
+    t.string "content_body"
+    t.integer "rating"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "reviews", "products"
 end
